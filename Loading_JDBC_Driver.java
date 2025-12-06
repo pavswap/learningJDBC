c758d507263ded8bf3e15a44b11679bc7a321ebf
Paavan Swaroop <paavanswaroop2@gmail.com>
2025-12-07 00:34:43 +0530

    Code to load MySql Driver

---
+++ Loading_JDBC_Driver.java
@@ -0,0 +1,13 @@
+class Loading_JDBC_Driver {
+
+    public static void main(String args[]) {
+    
+        try {
+          
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch {
+          
+            System.out.println(e.getMessage());
+        }
+    }
+}
