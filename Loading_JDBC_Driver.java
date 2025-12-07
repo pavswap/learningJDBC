101bc8b61b2b800216d8e1df5855c556d664138b
Paavan Swaroop <paavanswaroop2@gmail.com>
2025-12-07 19:27:43 +0530

    Code to load MySql Driver
    Code to load MySql Driver

--- Loading_JDBC_Driver.java
+++ Loading_JDBC_Driver.java
@@ -1,12 +1,10 @@
 class Loading_JDBC_Driver {
-
     public static void main(String args[]) {
-    
         try {
-          
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch {
-          
+            } catch(ClassNotFoundException e) {
+            
             System.out.println(e.getMessage());
         }
     }
